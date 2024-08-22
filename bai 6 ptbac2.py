640ba436a01d579a61a02440612bf6f6b49e67a0
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-22 20:14:19 +0700

    Add files via upload

---
+++ bai 6 ptbac2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 20:13:32 2024
+
+@author: nhuy23733881
+"""
+
+a = int(input("Nhap vao so a= "))
+b = int(input("Nhap vao so b= "))
+c = int(input("Nhap vao so c= "))
+
+print(f"{a}X^2 + {b}X + {c} = 0")
